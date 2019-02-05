d016464cdf6086959604f44ccc0139f036e85526
emily baier <emilyrbaier@gmail.com>
2019-02-04 16:34:06 -0800

    grab all user data except elephants

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const userName = document.getElementById('name');
 const jobPosition = document.getElementById('position');
 const allergy = document.getElementById('allergy');
 const allergySpan = document.getElementById('allergy-slider');
-const yesElephants = document.getElementById('yes');
-const noElephants = document.getElementById('no');
+// const yesElephants = document.getElementById('yes');
+// const noElephants = document.getElementById('no');
 
 
 
@@ -16,14 +16,19 @@ circusForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const tigerNames = [];
-
     for(let index = 0; index < circusForm.tiger.length; index++) {
         const name = circusForm.tiger[index];
         if(name.checked) {
             tigerNames[index] = name.value;
-            console.log(tigerNames[index]);
         }
     }
+    const applicant = {
+        name: userName.value,
+        position: jobPosition.value,
+        allergy: allergy.value,
+        tigerName: tigerNames
+    };
+    console.log(applicant);
 });
 
 
