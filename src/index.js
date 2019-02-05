f553b783ab7f47ce958cbd6ad7fd074ecfe7a4a0
emily baier <emilyrbaier@gmail.com>
2019-02-04 16:13:47 -0800

    added event listeners and loops to gather form info

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ const circusForm = document.getElementById('circus-app');
 const userName = document.getElementById('name');
 const jobPosition = document.getElementById('position');
 const allergy = document.getElementById('allergy');
-const allergyDisplay = document.getElementById('allergy-slider');
+const allergySpan = document.getElementById('allergy-slider');
 const yesElephants = document.getElementById('yes');
 const noElephants = document.getElementById('no');
 
-allergy.addEventListener('change', function(){
-    allergyDisplay.textContent = allergy.value;
+
+
+allergy.addEventListener('change', function() {
+    allergySpan.textContent = allergy.value;
 });
+
+circusForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const tigerNames = [];
+
+    for(let index = 0; index < circusForm.tiger.length; index++) {
+        const name = circusForm.tiger[index];
+        if(name.checked) {
+            tigerNames[index] = name.value;
+            console.log(tigerNames[index]);
+        }
+    }
+});
+
+
